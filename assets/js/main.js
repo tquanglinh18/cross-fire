48902b25e45a1533bdd7426feab34d63dff329e0
LinhTQ <linhtq@vtconline.vn>
2024-06-27 17:37:40 +0700

    home pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,26 @@
 $(function () {
   $("#btn-menu-mobile").click(() => {
-    $("#header-mobile").toggleClass("active");
+    $("#header-mobile").toggleClass("header-mobile-active");
   });
 });
+
+function tabOpen(idSection, idHeaderSelected, idContentSelected) {
+  var tabContent, tabHeader;
+  var idHeaderSelectedQuerry = "#" + idHeaderSelected;
+  var idContentSelectedQuerry = "#" + idContentSelected;
+
+  var idAllHeaderInSection = "#" + idSection + " .tabs__header--items";
+  var idAllContentsInSection = "#" + idSection + " .tabs__contents--items";
+  tabContent = $(idAllContentsInSection);
+  tabContent.each((index, value) => {
+    $(value).hide();
+  });
+
+  tabHeader = $(idAllHeaderInSection);
+  tabHeader.each((index, value) => {
+    $(value).removeClass("tabs__header--active");
+  });
+
+  $(idHeaderSelectedQuerry).addClass("tabs__header--active");
+  $(idContentSelectedQuerry).show().addClass("tabs__contents--active");
+}
