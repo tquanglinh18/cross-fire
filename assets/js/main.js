0fb713418e7fffaa3ef9353b0f8b76e24592242d
LinhTQ <linhtq@vtconline.vn>
2024-07-04 00:36:46 +0700

    fix switch tabs

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,8 +12,9 @@ function tabOpen(idSection, idHeaderSelected, idContentSelected) {
   var idAllHeaderInSection = "#" + idSection + " .tabs__header--items";
   var idAllContentsInSection = "#" + idSection + " .tabs__contents--items";
   tabContent = $(idAllContentsInSection);
+
   tabContent.each((index, value) => {
-    $(value).hide();
+    $(value).hide().removeClass("tabs__contents--active");
   });
 
   tabHeader = $(idAllHeaderInSection);
@@ -23,4 +24,26 @@ function tabOpen(idSection, idHeaderSelected, idContentSelected) {
 
   $(idHeaderSelectedQuerry).addClass("tabs__header--active");
   $(idContentSelectedQuerry).show().addClass("tabs__contents--active");
+}
+
+function tabSubOpen(idSection, idHeaderSelected, idContentSelected) {
+  var tabContent, tabHeader;
+  var idHeaderSelectedQuerry = "#" + idHeaderSelected;
+  var idContentSelectedQuerry = "#" + idContentSelected;
+
+  var idAllHeaderInSection = "#" + idSection + " .tabs__subHeader--items";
+  var idAllContentsInSection = "#" + idSection + " .tabs__subContents--items";
+  tabContent = $(idAllContentsInSection);
+
+  tabContent.each((index, value) => {
+    $(value).hide().removeClass("tabs__subContents--active");
+  });
+
+  tabHeader = $(idAllHeaderInSection);
+  tabHeader.each((index, value) => {
+    $(value).removeClass("tabs__subHeader--active");
+  });
+
+  $(idHeaderSelectedQuerry).addClass("tabs__subHeader--active");
+  $(idContentSelectedQuerry).show().addClass("tabs__subContents--active");
 }
