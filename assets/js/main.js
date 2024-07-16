733b3d446e8fea1a420a4cc346a0d2970e414430
LinhTQ <linhtq@vtconline.vn>
2024-07-16 17:39:35 +0700

    bug nhu cc

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,54 +13,11 @@ $(function () {
   });
 });
 
-function tabOpen(idSection, idHeaderSelected, idContentSelected) {
-  var tabContent, tabHeader;
-  var idHeaderSelectedQuerry = "#" + idHeaderSelected;
-  var idContentSelectedQuerry = "#" + idContentSelected;
-
-  var idAllHeaderInSection = "#" + idSection + " .tabs__header--items";
-  var idAllContentsInSection = "#" + idSection + " .tabs__contents--items";
-  tabContent = $(idAllContentsInSection);
-
-  tabContent.each((index, value) => {
-    $(value).hide().removeClass("tabs__contents--active");
-  });
-
-  tabHeader = $(idAllHeaderInSection);
-  tabHeader.each((index, value) => {
-    $(value).removeClass("tabs__header--active");
-  });
-
-  $(idHeaderSelectedQuerry).addClass("tabs__header--active");
-  $(idContentSelectedQuerry).show().addClass("tabs__contents--active");
-}
-
-function tabSubOpen(idSection, idHeaderSelected, idContentSelected) {
-  var tabContent, tabHeader;
-  var idHeaderSelectedQuerry = "#" + idHeaderSelected;
-  var idContentSelectedQuerry = "#" + idContentSelected;
-
-  var idAllHeaderInSection = "#" + idSection + " .tabs__subHeader--items";
-  var idAllContentsInSection = "#" + idSection + " .tabs__subContents--items";
-  tabContent = $(idAllContentsInSection);
-
-  tabContent.each((index, value) => {
-    $(value).hide().removeClass("tabs__subContents--active");
-  });
-
-  tabHeader = $(idAllHeaderInSection);
-  tabHeader.each((index, value) => {
-    $(value).removeClass("tabs__subHeader--active");
-  });
-
-  $(idHeaderSelectedQuerry).addClass("tabs__subHeader--active");
-  $(idContentSelectedQuerry).show().addClass("tabs__subContents--active");
-}
-
 function tabOpenHandle(idSection, idTabsActive) {
   var tabHeaderElm = $(
     "#".concat(idSection).concat(" > .tabs__header > .tabs__header--items")
   );
+
   var tabContentsElm = $(
     "#".concat(idSection).concat(" > .tabs__contents > .tabs__contents--items")
   );
@@ -73,22 +30,21 @@ function tabOpenHandle(idSection, idTabsActive) {
     $(value).removeClass("tabs__header--active");
   });
 
-  $(
-    "#"
-      .concat(idSection)
-      .concat(" > .tabs__header #")
-      .concat(idTabsActive)
-      .concat("-header")
-  ).addClass("tabs__header--active");
-  $(
-    "#"
-      .concat(idSection)
-      .concat(" > .tabs__contents #")
-      .concat(idTabsActive)
-      .concat("-contents")
-  )
-    .show()
-    .addClass("tabs__contents--active");
+
+  var tabHeaderSel = "#"
+    .concat(idSection)
+    .concat(" #")
+    .concat(idTabsActive)
+    .concat("-header");
+
+  var tabContentsSel = "#"
+    .concat(idSection)
+    .concat(" #")
+    .concat(idTabsActive)
+    .concat("-contents");
+
+  $(tabHeaderSel).addClass("tabs__header--active");
+  $(tabContentsSel).show().addClass("tabs__contents--active");
 }
 
 const initialActionClickTab = () => {};
