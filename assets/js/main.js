8b86af40eff2a944da708f792ee2a8aa815d6920
LinhTQ <linhtq@vtconline.vn>
2024-08-05 17:41:17 +0700

    cfs 2024

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 $(function () {
-
+  $("#btn-menu-mobile").click(() => {
+    if ($("#menu-mobile").css("display") == "none") {
+      $("#menu-mobile").show();
+    } else {
+      $("#menu-mobile").hide();
+    }
+  });
 });
