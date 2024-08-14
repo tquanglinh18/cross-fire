66ff7a9fab1b5c56c73120d20166ea7b9c1aaa9e
LinhTQ <linhtq@vtconline.vn>
2024-08-14 08:56:11 +0700

    check in cfs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
-
+  $("#btn-copy-hashtag").on("click", () => {
+    navigator.clipboard
+      .writeText("#CFSSC2024 , #CrossFire , #DotKich")
+      .then(() => alert("Copy Thành công"))
+      .catch(() => alert("Copy Không thành công"));
+  });
 });
