bcd047f0ab5d07c6bb03190bdd57d3425c547b49
LinhTQ <linhtq@vtconline.vn>
2024-12-23 20:01:22 +0700

    add: đăng ký nhanh

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
 $(function () {
+    $('.toggle-password').click(function () {
+        var passwordField = $('.toggle-password ~ input');
+        var passwordFieldType = passwordField.attr('type');
+        if (passwordFieldType === 'password') {
+            passwordField.attr('type', 'text');
+        } else {
+            passwordField.attr('type', 'password');
+        }
 
+        $('.toggle-password').toggleClass('toggle-password--show');
+        $('.toggle-password').toggleClass('toggle-password--hide');
+    });
 });
